0d303aa7f039d5a62cc9523aab0d0a8a680ac4b0
emily <emily@bacheconsulting.com>
2025-09-11 15:34:44 +0200

    [underway] - Discover Adapter Interface

--- csharp/Checklists/discover_interface.md
+++ csharp/Checklists/discover_interface.md
@@ -3,6 +3,7 @@ Discover Adapter Interface
 * Copy this checklist into the code and make a commit with message: [intention] - Discover Adapter Interface
 * Identify every call to 'wrapper.Awkward'. Go through each one and decide which checklist to use. Either 'Discover Adapter Method' or 'Propagate Adapter'. Add the relevant checklist as a comment.
 * Follow the new checklists for each call to 'wrapper.Awkward'.
+* Delete unnecessary comments and make a commit [completed] - Discover Adapter Interface
 
 Discover Adapter Method
 =======================
@@ -10,7 +11,7 @@ Use this checklist for code that uses wrapper.Awkward that you don't need to inc
 * 'Extract method' for the code using 'wrapper.Awkward'. Preferably take the whole line of code into the new method. Make sure the wrapper class is one of the arguments.
 * If the new method is not static, 'Introduce Parameter' for all the member variables it uses. Then make it static.
 * 'Make method non static' and select the wrapper class as the instance to move it to.
-* Delete unnecessary comments and make a commit [completed] - Discover Adapter Interface
+* Delete unnecessary comments and make a commit [underway] - Discover Adapter Interface
 
 Propagate Adapter
 =================
--- csharp/DiagramPrinter/DiagramPrinter.cs
+++ csharp/DiagramPrinter/DiagramPrinter.cs
@@ -5,6 +5,11 @@ namespace DiagramPrinter;
 public class FlowchartDiagramAdapter(FlowchartDiagram? diagram)
 {
     public FlowchartDiagram? Diagram { get; } = diagram;
+
+    public bool DiagramIsAvailable()
+    {
+        return Diagram != null;
+    }
 }
 
 /**
@@ -29,12 +34,17 @@ public class DiagramPrinter
      */
     public static bool PrintSummary(FlowchartDiagramAdapter flowchartDiagramAdapter, string language, out string summaryText)
     {
-        if (flowchartDiagramAdapter.Diagram == null)
+        if (!flowchartDiagramAdapter.DiagramIsAvailable())
         {
             summaryText = "";
             return false;
         }
 
+        // Use this checklist for code that uses wrapper.Awkward that you don't need to include in your unit tests.
+        // * 'Extract method' for the code using 'wrapper.Awkward'. Preferably take the whole line of code into the new method. Make sure the wrapper class is one of the arguments.
+        // * If the new method is not static, 'Introduce Parameter' for all the member variables it uses. Then make it static.
+        // * 'Make method non static' and select the wrapper class as the instance to move it to.
+        // * Delete unnecessary comments and make a commit [underway] - Discover Adapter Interface
         var summary = new DiagramSummary(language);
         summary.AddTitle(flowchartDiagramAdapter.Diagram.Name(), flowchartDiagramAdapter.Diagram.SerialNumber());
         summary.AddHeader(flowchartDiagramAdapter.Diagram.SummaryInformation());
