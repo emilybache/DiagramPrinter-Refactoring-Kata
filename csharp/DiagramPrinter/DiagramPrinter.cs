2b2fcfd66e654ea4763997101db29a43a0e491dc
emily <emily@bacheconsulting.com>
2025-09-11 15:36:08 +0200

    [completed] - Discover Adapter Interface

--- csharp/DiagramPrinter/DiagramPrinter.cs
+++ csharp/DiagramPrinter/DiagramPrinter.cs
@@ -10,6 +10,26 @@ public class FlowchartDiagramAdapter(FlowchartDiagram? diagram)
     {
         return Diagram != null;
     }
+
+    public string Name()
+    {
+        return Diagram.Name();
+    }
+
+    public string SerialNumber()
+    {
+        return Diagram.SerialNumber();
+    }
+
+    public string SummaryInformation()
+    {
+        return Diagram.SummaryInformation();
+    }
+
+    public PngDocument FlowchartThumbnail()
+    {
+        return Diagram.FlowchartThumbnail();
+    }
 }
 
 /**
@@ -26,12 +46,7 @@ public class DiagramPrinter
     {
         return PrintSummary(new FlowchartDiagramAdapter(diagram), language, out summaryText);
     }
-
-    /*
-* Copy this checklist into the code and make a commit with message: [intention] - Discover Adapter Interface
-* Identify every call to 'wrapper.Awkward'. Go through each one and decide which checklist to use. Either 'Discover Adapter Method' or 'Propagate Adapter'. Add the relevant checklist as a comment.
-* Follow the new checklists for each call to 'wrapper.Awkward'. 
-     */
+    
     public static bool PrintSummary(FlowchartDiagramAdapter flowchartDiagramAdapter, string language, out string summaryText)
     {
         if (!flowchartDiagramAdapter.DiagramIsAvailable())
@@ -39,16 +54,11 @@ public class DiagramPrinter
             summaryText = "";
             return false;
         }
-
-        // Use this checklist for code that uses wrapper.Awkward that you don't need to include in your unit tests.
-        // * 'Extract method' for the code using 'wrapper.Awkward'. Preferably take the whole line of code into the new method. Make sure the wrapper class is one of the arguments.
-        // * If the new method is not static, 'Introduce Parameter' for all the member variables it uses. Then make it static.
-        // * 'Make method non static' and select the wrapper class as the instance to move it to.
-        // * Delete unnecessary comments and make a commit [underway] - Discover Adapter Interface
+        
         var summary = new DiagramSummary(language);
-        summary.AddTitle(flowchartDiagramAdapter.Diagram.Name(), flowchartDiagramAdapter.Diagram.SerialNumber());
-        summary.AddHeader(flowchartDiagramAdapter.Diagram.SummaryInformation());
-        summary.AddImage(flowchartDiagramAdapter.Diagram.FlowchartThumbnail());
+        summary.AddTitle(flowchartDiagramAdapter.Name(), flowchartDiagramAdapter.SerialNumber());
+        summary.AddHeader(flowchartDiagramAdapter.SummaryInformation());
+        summary.AddImage(flowchartDiagramAdapter.FlowchartThumbnail());
         summaryText = summary.Export();
         return true;
     }
