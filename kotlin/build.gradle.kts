78d032ec80d1773b5bd269ba1762cdfbb965e954
emily <emily@bacheconsulting.com>
2025-09-25 07:57:45 +0200

    upgrade gradle

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -12,11 +12,16 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("com.approvaltests", "approvaltests", "24.2.0")
+    testImplementation(platform("org.junit:junit-bom:5.13.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(22)
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
